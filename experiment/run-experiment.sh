8ab416c57e902b8602fef6e81f561ca28dcd890a
thais <thaiscamachoo@gmail.com>
2020-06-07 14:30:52 -0300

    up

--- experiment/run-experiment.sh
+++ experiment/run-experiment.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 mkdir -p results
+cd ../examples
 
-/usr/bin/time -a -p mpiexec -n 1 python -m mpi4py TaylorGreen.py 1> TaylorGreen-1.out 2> TaylorGreen-1.err
+/usr/bin/time -o $bench-$size-TIME.out -v -p mpiexec -n 1 python -m mpi4py TaylorGreen.py 1> ../experiments/TaylorGreen-1.out 2> ../experiments/TaylorGreen-1.err
 #/usr/bin/time -a -p mpiexec -n 2 python -m mpi4py TaylorGreen.py 1> TaylorGreen-2.out 2> TaylorGreen-2.err
 #/usr/bin/time -a -p mpiexec -n 4 python -m mpi4py TaylorGreen.py 1> TaylorGreen-2.out 2> TaylorGreen-2.err
 #/usr/bin/time -a -p mpiexec -n 8 python -m mpi4py TaylorGreen.py 1> TaylorGreen-2.out 2> TaylorGreen-2.err
 
-/usr/bin/time -a -p mpiexec -n 1 python -m mpi4py 3Dadvect.py 1> 3Dadvect-1.out 2> 3Dadvect-1.err
+/usr/bin/time -o $bench-$size-TIME.out -v -p mpiexec -n 1 python -m mpi4py 3Dadvect.py 1> ../experiments/3Dadvect-1.out 2> ../experiments/3Dadvect-1.err
 #/usr/bin/time -a -p mpiexec -n 2 python -m mpi4py 3Dadvect.py 1> 3Dadvect-2.out 2> 3Dadvect-2.err
 #/usr/bin/time -a -p mpiexec -n 4 python -m mpi4py 3Dadvect.py 1> 3Dadvect-4.out 2> 3Dadvect-4.err
 #/usr/bin/time -a -p mpiexec -n 8 python -m mpi4py 3Dadvect.py 1> 3Dadvect-8.out 2> 3Dadvect-8.err
--- experiment/script-build.sh
+++ experiment/script-build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+pip install -r ../requirements.txt
+
 cd ../util/mpi4py-3.0.0
 python setup.py build --mpicc=/usr/bin/mpicc
 python setup.py install --prefix=install_location_mpi4py
@@ -7,5 +9,5 @@ cd ../..
 python setup.py build
 python setup.py install --prefix=install_location_pyranda
 
-export PYTHONPATH="$PWD/../util/mpi4py-3.0.0/install_location_mpi4py/lib/python2.7/site-packages:
-$PWD/../install_location_pyranda/lib/python2.7/site-packages:$PWD/../kernel_stats"
+export PYTHONPATH="$PWD/../util/mpi4py-3.0.0/install_location_mpi4py/lib/python2.7/site-packages:$PWD/../install_location_pyranda/lib/python2.7/site-packages:$PWD/../kernel_stats"
+
