0a76ac9f1afcda0586564c49a0abae778ad3524f
Thais <thaiscamachoo@gmail.com>
2020-06-15 18:43:44 -0300

    up

--- experiments/run-experiment.sh
+++ experiments/run-experiment.sh
@@ -3,4 +3,8 @@
 mkdir -p results
 cd ../examples
 
-mpiexec -n 1 python -m mpi4py 3Dadvect.py 1> ../experiments/results/3Dadvect-1.out 2> ../experiments/results/4Dadvect-1.err
+/usr/bin/time -o ../experiments/results/3Dadvect-TIME-2.out -p mpiexec -n 2 python -m mpi4py 3Dadvect.py 1> ../experiments/results/3Dadvect-2.out 2> ../experiments/results/3Dadvect-2.err
+
+/usr/bin/time -o ../experiments/results/3Dadvect-TIME-4.out mpiexec -n 4 python -m mpi4py 3Dadvect.py 1> ../experiments/results/3Dadvect-4.out 2> ../experiments/results/3Dadvect-4.err
+
+/usr/bin/time -o ../experiments/results/3Dadvect-TIME-8.out -p mpiexec -n 8 python -m mpi4py 3Dadvect.py 1> ../experiments/results/3Dadvect-8.out 2> ../experiments/results/3Dadvect-8.err
