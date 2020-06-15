a03a60b446b625335ddcf5255c7d112769cbd321
thais <thaiscamachoo@gmail.com>
2020-06-15 18:19:48 -0300

    fixed

--- kernel_stats/kernelStats.py
+++ kernel_stats/kernelStats.py
@@ -15,6 +15,7 @@ class kernelStats:
         self.begin_time = 0
         self.end_time = 0
         self.pi_sum = 0
+        self.pi = 0
         self. early_stop = early_stop
 
     def getCurrentTime(self):
@@ -39,6 +40,9 @@ class kernelStats:
         self.print_timestep(self.PRINT_EXIT, current_time);
 
     def beginTimestep(self):
+        begin_time_old = self.begin_time;
+        self.begin_time = self.getCurrentTime();
+
         if(self.current_iteration == 0):
             current_time = self.getCurrentTime()
 
@@ -46,23 +50,28 @@ class kernelStats:
 
             if(self.stop_in == 0):
                 self.exit()
+        else:
+            self.pi = self.end_time - begin_time_old
+            self.pi += self.begin_time - self.end_time
+            self.pi_sum += self.pi
+            self.print_timestep(self.PRINT_STATS, self.begin_time);
+        self.exit()
 
         self.current_iteration += 1
-        self.begin_time = self.getCurrentTime();
 
     def endTimestep(self):
         self.end_time = self.getCurrentTime()
-        self.print_timestep(self.PRINT_STATS, self.end_time);
-        self.pi_sum += self.end_time - self.begin_time
-        self.exit()
+        #self.print_timestep(self.PRINT_STATS, self.end_time);
+        #self.pi_sum += self.end_time - self.begin_time
+        #self.exit()
 
     def print_timestep(self, ptype, collected_time):
         rank = MPI.COMM_WORLD.Get_rank()
 
         if(ptype == self.PRINT_INIT):
-           print("[MO866] Init time,{},{}".format(rank, collected_time - self.init_time))
+           print("[MO833] Init time,{},{}".format(rank, collected_time - self.init_time))
         elif(ptype == self.PRINT_STATS):
-           print("[MO833] Paramount Iteration,{},{},{},{}".format(rank, self.current_iteration, collected_time - self.begin_time, collected_time - self.init_time))
+           print("[MO833] Paramount Iteration,{},{},{},{}".format(rank, self.current_iteration, collected_time - self.init_time, self.pi))
         elif(ptype == self.PRINT_EXIT):
            print("[MO833] Total time,{},{}".format(rank,collected_time - self.init_time))
         elif(ptype == self.PRINT_AVG):
