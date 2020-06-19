79110603a414a04b562c13b823094c84e4992eb0
Thais <thaiscamachoo@gmail.com>
2020-06-19 16:02:47 -0300

    up

--- experiments/run-experiment.sh
+++ experiments/run-experiment.sh
@@ -7,7 +7,7 @@ cd ../examples
 for bench in 3Dadvect TaylorGreen; do
   for threads in 1 2 4 8; do
     now=$(date +"%m-%d-%y-%T")
-    exp_path=$path/$bench/${threads}node06/$now
+    exp_path=$path/$bench/node06-${threads}/$now
     mkdir -p $exp_path
     /usr/bin/time -o $exp_path/results.time -p mpiexec -n $threads python -m mpi4py ${bench}.py 1> $exp_path/results.out 2> $exp_path/results.err
     mv result.png $exp_path/results.png
