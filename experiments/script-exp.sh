d934781af87540e7260566873b6e42c9d314edd5
Thais <thaiscamachoo@gmail.com>
2020-06-28 17:06:03 -0300

    results clean

---
+++ experiments/script-exp.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+mkdir -p results
+path=$PWD/results
+cd ../examples
+
+for bench in 3Dadvect; do
+  for threads in 1 2 4 8; do
+    now=$(date +"%m-%d-%y-%T")
+    exp_path=$path/$bench/node06-${threads}/$now
+    mkdir -p $exp_path
+    /usr/bin/time -o $exp_path/results.time -p mpiexec -n $threads python -m mpi4py ${bench}.py 1> $exp_path/results.out 2> $exp_path/results.err
+    mv result.png $exp_path/results.png
+  done
+done
+
+for bench in gatech; do
+  for threads in 1 2 4 8; do
+    now=$(date +"%m-%d-%y-%T")
+    exp_path=$path/$bench/node06-${threads}/$now
+    mkdir -p $exp_path
+    /usr/bin/time -o $exp_path/results.time -p mpiexec -n $threads python -m mpi4py ${bench}.py 1> $exp_path/results.out 2> $exp_path/results.err
+  done
+done
